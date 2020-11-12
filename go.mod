5f9b3f9af5f1b8b3500cdb240679f1171ef75271
skanehira <sho19921005@gmail.com>
2020-11-12 23:03:55 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/skanehira/gyazo
+
+go 1.15
+
+require (
+	github.com/skanehira/clipboard-image/v2 v2.0.0 // indirect
+	github.com/tomohiro/go-gyazo v0.0.0-20191210111541-99e3a144af58 // indirect
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/skanehira/clipboard-image/v2"
+	"github.com/tomohiro/go-gyazo/gyazo"
+)
+
+var version = "0.0.1"
+
+var token string
+
+func getToken() (string, error) {
+	token := os.Getenv("GYAZO_TOKEN")
+	if token != "" {
+		return token, nil
+	}
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configFile := filepath.Join(dir, ".gyazo_token")
+	b, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return "", err
+	}
+
+	token = strings.Trim(string(b), "\r\n")
+
+	if token == "" {
+		return "", errors.New("gyazo token is empty")
+	}
+
+	return token, nil
+}
+
+func run(args []string) error {
+	var (
+		r   io.Reader
+		err error
+	)
+
+	token, err = getToken()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if len(args) == 0 {
+		r, err = clipboard.Read()
+		if err != nil {
+			return err
+		}
+	} else {
+		r, err = os.Open(args[0])
+		if err != nil {
+			return err
+		}
+	}
+
+	image, err := upload(r)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(image.URL)
+	return nil
+}
+
+func upload(r io.Reader) (*gyazo.Image, error) {
+	gyazo, err := gyazo.NewClient(token)
+	if err != nil {
+		return nil, err
+	}
+	image, err := gyazo.Upload(r)
+	if err != nil {
+		return nil, err
+	}
+	return image, nil
+}
+
+func main() {
+	name := "gyazo"
+	fs := flag.NewFlagSet(name, flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.Usage = func() {
+		fs.SetOutput(os.Stdout)
+		fmt.Printf(`%[1]s - Gyazo CLI
+
+VERSION: %s
+
+USAGE:
+  $ %[1]s [file]
+
+EXAMPLE:
+  $ %[1]s gorilla.png
+  $ %[1]s 
+`, name, version)
+	}
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		if err == flag.ErrHelp {
+			return
+		}
+		os.Exit(1)
+	}
+
+	if err := run(fs.Args()); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
